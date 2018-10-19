f0e1b7bbbf861b9917485fde9135a5bb73d41720
da1271 <dwyleahn@gmail.com>
2018-10-19 19:08:08 -0400

    updates forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,71 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  renderField(field) {
     return (
-      <div>
-        PostsNew!
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      {field.meta.error}
       </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+// name is what connects to error function
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  //validate the inputs from 'values'
+  if(!values.title || values.title.length < 3) {
+    errors.title = "Enter a title that is at least 3 characters!";
+  }
+  if(!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if(!values.content) {
+    errors.content = "Enter some content!";
+  }
+  // if errors is empty, the form is fine to submit
+  // if errors has *any properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm" // name of the form: has to be unique
+})(PostsNew);
